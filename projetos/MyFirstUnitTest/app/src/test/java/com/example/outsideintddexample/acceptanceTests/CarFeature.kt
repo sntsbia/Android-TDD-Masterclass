3a0fd48d23466fedeaf1b3433b15bab8e011d688
sntsb <sntsb.doc@gmail.com>
2025-01-29 11:23:33 -0300

    Finalizando seção 2 - testes com mocks

--- projetos/MyFirstUnitTest/app/src/test/java/com/example/outsideintddexample/acceptanceTests/CarFeature.kt
+++ projetos/MyFirstUnitTest/app/src/test/java/com/example/outsideintddexample/acceptanceTests/CarFeature.kt
@@ -8,26 +8,29 @@ import org.junit.Test
 
 class CarFeature {
 
-//    private val engine = Engine()
-//
-//    private val car = Car(6.0, engine)
-//
-//    @Test
-//    fun carIsLoosingFuelWhenItTurnsOn() {
-//
-//        car.turnOn()
-//
-//        assertEquals(5.5, car.fuel)
-//    }
-//
-//    @Test
-//    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
-//
-//        car.turnOn()
-//
-//        assertEquals(95, car.engine.temperature)
-//        assertTrue(car.engine.isTurnedOn)
-//
-//    }
+    private val engine = Engine()
+
+    private val car = Car(
+        engine,
+        6.0
+    )
+
+    @Test
+    fun carIsLoosingFuelWhenItTurnsOn() {
+
+        car.turnOn()
+
+        assertEquals(5.5, car.fuel)
+    }
+
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
+
+        car.turnOn()
+
+        assertEquals(95, car.engine.temperature)
+        assertTrue(car.engine.isTurnedOn)
+
+    }
 
 }
---
+++ projetos/MyFirstUnitTest/app/src/test/java/com/example/outsideintddexample/unitTests/EngineShould.kt
@@ -0,0 +1,27 @@
+package com.example.outsideintddexample.unitTests
+
+import com.example.outsideintddexample.model.Engine
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class EngineShould {
+
+    private val engine = Engine()
+
+    @Test
+    fun turnOn() {
+        engine.turnOn()
+
+        assertTrue(engine.isTurnedOn)
+    }
+
+    @Test
+    fun riseTemperatureWhenItTurnsOn() {
+        engine.turnOn()
+
+        assertEquals(95, engine.temperature)
+
+    }
+
+}
